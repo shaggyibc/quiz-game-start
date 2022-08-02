81232f2e8b52bf37664df89cd47cda7c8e13ef01
shaggyibc <102387577+shaggyibc@users.noreply.github.com>
2022-08-01 20:12:37 -0700

    added random choice to questions

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from question_model import Question
 from quiz_brain import QuizBrain
 from data import question_data
+import random
 
 question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
+# unused_questions = question_data
+for x in range (1, len(question_data)):
+    random_question = random.choice(question_data)
+    del question_data[question_data.index(random_question)]
+    question_text = random_question["question"]
+    question_answer = random_question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
